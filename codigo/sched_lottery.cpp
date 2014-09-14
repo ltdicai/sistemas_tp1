66ffc3f7185160b4e07f8d11d1909270dece1d4d
fzanollo <fzanollo@dc.uba.ar>
2014-09-13 23:39:19 -0300

    schedLottery not working - segm fault

--- codigo/sched_lottery.cpp
+++ codigo/sched_lottery.cpp
@@ -3,10 +3,26 @@
 using namespace std;
 
 SchedLottery::SchedLottery(vector<int> argn) {
-  // FCFS recibe la cantidad de cores.
+  //suponemos argn[0]=cant_cores; argn[1]=quantum; argn[2]=semilla;
+
+	cout << "la concha de tu madre all boys";
+	cout << argn[0] << " " << argn[1] << " " << argn[2];
+	cantCpu = argn[0];
+	quantumMaximo = argn[1];
+	int semilla = argn[2];
+	Task idle = Task(IDLE_TASK, -1);
+	//inicializo taskList de cores
+	for(int i = 0; i < cantCpu; i++){ //uno por cada core
+		
+		quantumActualDeCpu[i] = 0;//Inicializo todo en 0
+		tareaActualDeCpu[i] = idle; //al comienzo todas las cpu están corriendo IDLE
+	}
+	//randomizo con la semilla recibida
+	srand(semilla);
 }
 
 SchedLottery::~SchedLottery() {
+
 }
 
 void SchedLottery::load(int pid) {
@@ -14,11 +30,63 @@ void SchedLottery::load(int pid) {
 }
 
 void SchedLottery::load(int pid,int deadline) {
+	//Llega una tarea
+	//Lo encolo en la lista de tareas
+	cout << endl << deadline << endl;
+	int cantTickets;
+	if(deadline == 0){
+		cantTickets = 1;
+	}
+	else{
+		cantTickets = floor(10/deadline) + 1; //La cantidad de tickets otorgados es proporcional al deadline 
+	}
+	totalTickets += cantTickets; //Aumento la cantidad de tickets
+	Task nuevaTarea = Task(pid,cantTickets);//Creo nueva tarea
+	readyTasks.push_back(nuevaTarea); //Inserto nueva tarea a la lista de tareas por ejecutar
 }
 
 void SchedLottery::unblock(int pid) {
+	Task desbloqueada = tareasBloqueadas[pid];
+	readyTasks.push_back(desbloqueada);
 }
 
 int SchedLottery::tick(int cpu, const enum Motivo m) {
-  return 0;
+	if(tareaActualDeCpu[cpu].pid == IDLE_TASK){ 
+	//si estoy corriendo IDLE
+		tareaActualDeCpu[cpu] = lottery(); //Hacemos la loteria
+	}
+	else{
+		if(m == TICK){
+			quantumActualDeCpu[cpu]++;//Sumo 1 al quantum del cpu actual
+			if(quantumActualDeCpu[cpu] == quantumMaximo){ 
+			//si se le terminó el quantum a la tarea actual
+				readyTasks.push_back(tareaActualDeCpu[cpu]); //la pongo en ready 
+				quantumActualDeCpu[cpu] = 0; //vuelvo el quantum a cero
+				tareaActualDeCpu[cpu] = lottery(); //Hacemos la loteria
+			}
+			///si no se le terminó el quantum a la tarea actual sigue ejecutando
+		}
+		else{ //BLOCK o EXIT
+			if(m == BLOCK){
+				//Compenso Tickets()
+				tareasBloqueadas.insert(std::pair<int, Task>(tareaActualDeCpu[cpu].pid, tareaActualDeCpu[cpu])); 
+				//guardo <pid, Task> en tareas_bloqueadas
+
+			}
+			quantumActualDeCpu[cpu] = 0; //vuelvo el quantum a cero
+			tareaActualDeCpu[cpu] = lottery(); //Hacemos la loteria
+		}
+	}
+  return tareaActualDeCpu[cpu].pid;
+}
+
+Task SchedLottery::lottery(){
+	int ticketDorado = floor((rand()/RAND_MAX)*totalTickets + 1); 
+	int sumaTickets = 0;
+	unsigned int it = 0;
+	while(it < readyTasks.size() && sumaTickets < ticketDorado){
+		sumaTickets += readyTasks[it].cantTickets;
+		it++; 
+	}
+	return readyTasks[it];
 }
--- codigo/sched_lottery.h
+++ codigo/sched_lottery.h
@@ -2,7 +2,18 @@
 #define __SCHED_LOT__
 
 #include "basesched.h"
+#include <map>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
 
+struct Task
+{
+  Task():pid(0),cantTickets(-1){};
+	Task(int pid, int cantTickets): pid(pid), cantTickets(cantTickets){};
+	int pid;
+	int cantTickets;
+};
 
 class SchedLottery : public SchedBase {
   public:
@@ -12,6 +23,19 @@ class SchedLottery : public SchedBase {
     virtual void load(int pid, int deadline);
     virtual void unblock(int pid);
     virtual int tick(int cpu, const enum Motivo m);
+
+  private:
+
+  	std::vector<Task> readyTasks;
+  	std::vector<Task> tareaActualDeCpu;
+  	std::vector<int> quantumActualDeCpu;
+  	
+  	int quantumMaximo;
+  	int totalTickets;
+  	int cantCpu;
+    std::map<int,Task> tareasBloqueadas; //map<pid, Task> for blocked tasks
+
+    Task lottery();
 
 };
 
